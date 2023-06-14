33dbb5d792bb6d2ed3142c2bc22cbf33edfc2169
unknown <kgangireddy04@gmail.com>
2023-06-14 12:42:14 -0400

    first edit

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print('Can you see this?')
+import os
+import numpy as np
+import sys
+import webbrowser as wb
+
+User = input("Enter user of program here: ") 
+
+if (User == "Rushi" or "Akanksha"):
+    print(f"Welcome, {User}, to your personalized workspace!")  
+    wb.open('https://chat.openai.com/', new = 2)
+
