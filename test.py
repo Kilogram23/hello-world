f8e830b738673eeeacb174ad3efb077a368c59c4
unknown <kgangireddy04@gmail.com>
2023-06-16 10:40:44 -0400

    different parsing method

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import os
-import numpy as np
+import json
 import sys
 import webbrowser as wb
 
 User = input("Enter user of program here: ") 
+userdata = {}
 
 if (User == "Rushi" or "Akanksha"):
     print(f"Welcome, {User}, to your personalized workspace!")  
     wb.open('https://chat.openai.com/', new = 2)
+    userdata[User] = 'https://chat.openai.com/'
 
+
+
+with open('userdata.txt', 'w') as convert_file:
+     convert_file.write(json.dumps(userdata))
