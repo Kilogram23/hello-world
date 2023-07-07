f6d433454bfd2347f632726b9234a4fa1e6a2db9
Parshva Mehta <parshvam7@gmail.com>
2023-07-06 20:51:31 -0400

    Created new working file, read comments

---
+++ workspaces.py
@@ -0,0 +1,40 @@
+import os
+import json
+import sys
+import webbrowser as wb
+
+userdata = {}
+
+"""
+Todo List:
+- Create user function
+    - Attributes: (can use a login type interface)
+        - Name
+        - Major
+        - Age
+        - Email ID
+
+- Update user function
+    - Attributes: (Services provided)
+        - Open up workspaces
+        - Create macros ? 
+        - Brainstorm more ideas 
+- Call user function
+    - Self explanatory, access database to call based on inputs from GUI
+"""
+
+
+# Main function, used to call helper functions
+def main():
+    print("Hello World!")
+
+# Add user function, collect details on user --> Not sure how to implement just yet
+def addUser():
+    print("Hello World")
+
+#Update user function, update any user details or any links to add for workspace
+def updateUser():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
